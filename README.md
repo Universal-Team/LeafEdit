72b0faa11143d32c0f69da98ddf84b3fb8fbf5fd
SuperSaiyajinVoltZ <47382115+SuperSaiyajinVoltZ@users.noreply.github.com>
2019-08-27 10:06:32 +0200

    Add Credits to NLTK & Developers.

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ This is a work in progress Animal Crossing : New Leaf Save Manager & Editor for 
 - [Epicpkmn11](https://github.com/Epicpkmn11): Japanese & English
 - [VoltZ](https://github.com/SuperSaiyajinVoltZ): German & English
 ### Others
+
+- [Cuyler](https://github.com/Cuyler36), [Slattz](https://github.com/Slattz), [NLTK](https://github.com/Slattz/NLTK) : For the Core part.
 - [devkitPro](https://github.com/devkitPro), [Fincs](https://github.com/fincs), [Smealum](https://github.com/smealum), [Wintermute](https://github.com/WinterMute) : For devkitARM, Citro2D, Citro3D and Libctru.
 - [Flagbrew](https://github.com/FlagBrew): [Checkpoint](https://github.com/FlagBrew/Checkpoint)'s Code for the Backup & Restore part for Animal Crossing: New Leaf.
 - [Flagbrew](https://github.com/FlagBrew): [PKSM](https://github.com/FlagBrew/PKSM)'s Title Code, to get the Title ID for Animal Crossing : New Leaf and such.
--- include/core/save/offsets.h
+++ include/core/save/offsets.h
@@ -1,3 +1,25 @@
+/*
+MIT License
+Copyright (c) 2018-2019 Slattz, Cuyler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #pragma once
 
 #ifndef OFFSETS_H
@@ -334,16 +356,16 @@ typedef struct mPlayer
     //PLAYER_CATALOG=		0x6c90,
     //PLAYER_BANK=			0x6b8c,
     //PLAYER_MEDALS=		0x6b9c,
-    //PLAYER_WALLET=			0x6f08,
+    //PLAYER_WALLET=		0x6f08, 
     //PLAYER_ISLANDBOX=		0x6f10,
     
-    //PLAYER_LETTERS=			0x7008,
+    //PLAYER_LETTERS=		0x7008,
     //PLAYER_EMOTIONS=		0x89d0,
     //PLAYER_MEOW=			0x8d1c,
     //PLAYER_DRESSERS=		0x92f0,
     //PLAYER_EXTERIORS=		0x5D904,
     //PLAYER_ROOMS=			0x05d97a,
-    //PLAYER_STORAGE=			0x07a778,
+    //PLAYER_STORAGE=		0x07a778,
 } ACNL_Player;
 
 #endif
