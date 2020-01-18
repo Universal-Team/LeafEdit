e2fd8da9dc00b57ccbab1767337ed80e22d7e017
SuperSaiyajinStackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2020-01-18 22:39:54 +0100

    Create `3ds` folder for the 3ds version.

---
+++ 3ds/include/core/gameLoader.hpp
@@ -0,0 +1,44 @@
+	/*
+*   This file is part of LeafEdit
+*   Copyright (C) 2019-2020 DeadPhoenix8091, Epicpkmn11, Flame, RocketRobz, StackZ, TotallyNotGuy
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+*       * Requiring preservation of specified reasonable legal notices or
+*         author attributions in that material or in the Appropriate Legal
+*         Notices displayed by works containing it.
+*       * Prohibiting misrepresentation of the origin of that material,
+*         or requiring that modified versions of such material be marked in
+*         reasonable ways as different from the original version.
+*/
+
+#ifndef GAMELOADER_HPP
+#define GAMELOADER_HPP
+
+#include <3ds.h>
+#include <memory>
+
+namespace GameLoader
+{
+	// The Card / Installed Title checkers.
+	bool checkTitle(u64 TitleID);
+	bool scanCard(u64 TitleID);
+
+	// Check for the update of the regular AC:NL version, because editor only works with.
+	void checkUpdate(void);
+	void updateCheck2(void);
+};
+
+#endif
---
+++ 3ds/source/lang/lang.cpp
@@ -0,0 +1,48 @@
+#include "common/config.hpp"
+
+#include "lang/lang.hpp"
+
+#include <stdio.h>
+
+nlohmann::json appJson;
+
+std::vector<std::string> g_groups;
+std::vector<std::string> g_villagerDatabase;
+
+// Load a Text file or such to a Vector.
+static void loadToVector(std::string path, std::vector<std::string> &vec) {
+	char* line = NULL;
+	size_t len = 0;
+	vec.clear();
+
+	FILE* in = fopen(path.c_str(), "r");
+	while(__getline(&line, &len, in) != -1) {
+		if(line[strlen(line)-1] == '\n')    line[strlen(line)-1] = '\0';
+		vec.push_back(line);
+	}
+	fclose(in);
+}
+
+std::string Lang::get(const std::string &key) {
+	if(!appJson.contains(key)) {
+		return "MISSING: " + key;
+	}
+	return appJson.at(key).get_ref<const std::string&>();
+}
+
+std::string langs[] = {"de", "en", "es", "fr", "it", "lt", "pt", "jp"};
+
+void Lang::load(int lang) {
+	loadToVector("romfs:/lang/"+langs[1]+"/groups.txt", g_groups);
+	loadToVector("romfs:/lang/"+langs[lang]+"/villager.txt", g_villagerDatabase);
+	
+	FILE* values;
+	
+	if (Config::getInt("LangLocation") == 1) {
+		values = fopen(("sdmc:/LeafEdit/lang/"+langs[lang]+"/app.json").c_str(), "rt");
+	} else {
+		values = fopen(("romfs:/lang/"+langs[lang]+"/app.json").c_str(), "rt");
+	}
+	if(values)    appJson = nlohmann::json::parse(values, nullptr, false);
+	fclose(values);
+}
